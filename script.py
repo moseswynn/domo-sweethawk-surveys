fcb6a990b1b9e3af28da2ed11ecc31a1a7d661c4
Wynn, Moses <moses.wynn@team.neustar>
2022-06-27 16:29:27 -0400

    comments

--- script.py
+++ script.py
@@ -6,23 +6,29 @@ from io import StringIO
 from os import getenv
 import json
 
+# load current config from the conf.json file in the project directory
 with open('conf.json') as f:
     CONF = json.loads(f.read())
 
+# initialize domo client with domo API credentials
 domo = Domo(**CONF["creds"])
 
+# download the data from sweethawk and normalize
 data = pd.read_csv(CONF["csv_url"])
 data['date'] = pd.to_datetime(data['date'])
 data['reason'] = data['reason'].replace(np.nan, '')
 data['nps_reason'] = data['nps_reason'].replace(np.nan,'')
 
+# export to csv held in StringIO 
 csv = StringIO()
-
 data.to_csv(csv, index=False)
 
+
 if "dsid" in CONF.keys():
+    # skip this if the dataset id is already defined in conf.json
     pass
 else:
+    # create a new dataset if dsid is not defined and save the dsid to conf.json
     dsr = DataSetRequest()
     dsr.name = 'ZenDesk Sweethawk Surveys'
     dsr.description = 'Zendesk Surveys Exported from Sweethawk'
@@ -41,4 +47,5 @@ else:
     with open('conf.json', 'w') as f:
         f.write(json.dumps(CONF))
 
+# push the data to the domo dataset
 domo.datasets.data_import(CONF['dsid'], csv.getvalue())
